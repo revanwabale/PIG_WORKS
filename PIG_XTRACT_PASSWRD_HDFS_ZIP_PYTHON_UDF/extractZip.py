3879fe37a72493d105e82f510d934dba95577ee5
revanwabale <wabale.revan@gmail.com>
2018-10-29 16:11:27 +0900

    latest copy!

--- PIG_XTRACT_PASSWRD_HDFS_ZIP_PYTHON_UDF/extractZip.py
+++ PIG_XTRACT_PASSWRD_HDFS_ZIP_PYTHON_UDF/extractZip.py
@@ -11,7 +11,7 @@ def getPercent(data, ecu, pnumber, trip_log_file):
         #print 'Outside data[HeaderProgNo]={0}'.format(data[HeaderProgNo])
         #pnumber = data[HeaderProgNo]
 
-        if ecu == '614':
+        if ecu == 'xxx':
             if (pnumber is not None and pnumber == '51') and len(data) >= 416 and data[408] is not None and data[408] == '61' and data[409] is not None and data[409] == '61':
                 if data[415] is not None and data[416] is not None:
                     if len(data[415]) < 2:
@@ -28,7 +28,7 @@ def getPercent(data, ecu, pnumber, trip_log_file):
                         data[190]='0'+data[190] 
                     return round((int(data[189] + data[190], 16) * 0.0001), 4)*100
 	    
-        if ecu == '733':
+        if ecu == 'xx':
             if len(data) >= 473 and data[465] is not None and data[465] == '61' and data[466] is not None and data[466] == '61':
                 if data[472] is not None and data[473] is not None:
 		    if len(data[472]) < 2:
@@ -42,7 +42,7 @@ def getPercent(data, ecu, pnumber, trip_log_file):
 
 def getBAR(data, ecu, pnumber, trip_log_file):
     try:
-        if ecu == '617':
+        if ecu == 'xx':
             if len(data) >= 87 and data[1] is not None and data[1] == '61' and data[2] is not None and data[2] == '1' and data[87] is not None:
 	        bar = "{0:b}".format(int(data[87], 16))[:-3]
                 if bar is None or bar =='':
@@ -65,12 +65,12 @@ def executeHdfs(args):
 @outputSchema("t:tuple(percent:chararray,bar:chararray)")
 def getValues(ecu_id, trip_log_file, password, p_number):
     try:
-	if ecu_id == '614':
-            password = 'bc293a0'
-        if ecu_id == '733':
-            password = 'bc293a0'
-        if ecu_id == '617':
-            password = '0220qtn1'
+	if ecu_id == 'xx':
+            password = 'xx'
+        if ecu_id == 'xx':
+            password = 'xx'
+        if ecu_id == 'xx':
+            password = 'xx'
         #Verifying the file presence
         #print '\n raw_file_name_trip_log_file: ',trip_log_file
         checkFile = executeHdfs(['hdfs', 'dfs', '-test', '-e', trip_log_file])
@@ -115,86 +115,41 @@ def getValues(ecu_id, trip_log_file, password, p_number):
 #def main():
     #print("in main function.....!")
     """
-    #TEST : ECU 614 Percent based on new Program Logic..
-    ecu_id='614'
-    trip_log_file='/common/20006_SaW/10895/sigma_bk/raw_recv/abinitio_process_time=20180717/VIN(JN1FAAZE0U0012037)_20180713175719_3_ITA_________________1IKCA85695___ZE0___57.hst.zip'
-    password='bc293a0'
-    #password='0220qtn1'
-    p_number='50'
+    #TEST : ECU xx Percent based on new Program Logic..
+    ecu_id='xx'
+    trip_log_file='xx'
+    password='xx'
+    #password='xx'
+    p_number='xx'
     """
 
     """
-    #TEST : ECU 614 Percent based on new Program Logic.., known passed Examle, UT.
-    ecu_id='614'
-    trip_log_file='/common/20006_SaW/10895/sigma_bk/raw_recv/abinitio_process_time=20180717/VIN(JN1FAAZE0U0009057)_20180713104603_3_GER______________
-___6FKCA62619___ZE0___57.hst.zip'
-    password='bc293a0'
-    #password='0220qtn1'
-    p_number='50'
+    #TEST : ECU xx Percent based on new Program Logic.., known passed Examle, UT.
+    ecu_id='xx'
+    trip_log_file='xxx'
+    password='xx'
+    #password='xx'
+    p_number='xx'
     """
 
     """
-    #TEST : ECU 614 Percent based on new Program Logic.., known passed Examle, UT.
-    ecu_id='614'
-    trip_log_file='/common/20006_SaW/10895/sigma_bk/raw_recv/abinitio_process_time=20180717/VIN(JN1FAAZE0U0009057)_20180713104603_3_GER_________________6FKCA62619___ZE0___57.hst.zip'
-    password='bc293a0'
-    #password='0220qtn1'
-    p_number='50'
+    #TEST : ECU xx Percent based on new Program Logic.., known passed Examle, UT.
+    ecu_id='xx'
+    trip_log_file='xx'
+    password='xx'
+    #password='xx'
+    p_number='xx'
     """
 
     """
-    #TEST : ECU 617 bar based on new Program Logic..(UT Example) --> PASSED
-    ecu_id='617'
-    trip_log_file='/common/20006_SaW/10895/sigma_bk/raw_recv/abinitio_process_time=20180717/VIN(JN1FAAZE0U0012037)_20180713175719_3_ITA_________________1IKCA85695___ZE0____3.hst.zip'
-    #password='bc293a0'
-    password='0220qtn1'
-    p_number='15'
+    #TEST : ECU xx bar based on new Program Logic..(UT Example) --> PASSED
+    ecu_id='xx'
+    trip_log_file='xx'
+    #password='xx'
+    password='xx'
+    p_number='xx'
     """
-
-
-    """
-    #TEST 1:bar no retrived only percent retrived issue.
-    ecu_id='614'
-    trip_log_file='/common/20006_SaW/10895/sigma/raw_recv/abinitio_process_time=20180619/VIN(JN1FAAZE0U0016101)_20180618081248_3_NOR_________________2HKCA67786___ZE0___57.hst.zip'
-    password='bc293a0'
-    #password='0220qtn1'
-    p_number='51'
-    """
-
-    """
-    #TEST 1:
-    ecu_id='614'
-    trip_log_file='/common/20006_SaW/10895/sigma/raw_recv/abinitio_process_time=20180619/VIN(JN1FAAZE0U0005634)_20180618143156_3_EGB_________________3CKCA09994___ZE0____3.hst.zip'
-    #password='bc293a0'
-    password='0220qtn1'
-    p_number='50'
-    #p_number='51'
-    """
-
-    """
-    #TEST 2:
-    ecu_id='617'
-    trip_log_file='/common/20006_SaW/10895/sigma/raw_recv/abinitio_process_time=20180619/VIN(JN1FAAZE0U0010763)_20180615134736_3_SWI_________________2FKCA54779___ZE0___57.hst.zip'
-    #password='0220qtn1'
-    password='bc293a0'
-    p_number='15'
-    """
-
-    """
-    #TEST 3:
-    ecu_id='617'
-    trip_log_file='/common/20006_SaW/10895/sigma/raw_recv/abinitio_process_time=20180619/VIN(SJNFAAZE0U6002454)_20180618103254_3_EGB_________________5HKCA37765___ZE0___57.hst.zip'
-    password='bc293a0'
-    p_number='15'
-    """
-
-    """
-    #TEST 4: Exception File -> 
-    ecu_id='617'
-    trip_log_file='/common/20006_SaW/10895/sigma/raw_recv/abinitio_process_time=20180619/VIN(SJNFAAZE0U6003519)_20180618082220_3_NOR_________________3KKCA51972___ZE0____3.hst.zip'
-    password='0220qtn1'
-    p_number='15'
-    """
+ 
 
     #percent, bar = getValues(ecu_id, trip_log_file, password, p_number)
     #print 'bar={0} & Percent={1}'.format(bar, percent)
